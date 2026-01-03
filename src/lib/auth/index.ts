1a2c8925dcccfb4091af422283069c42df0e1ffe
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-01-03 20:11:00 +0500

    feat(auth): integrate email OTP functionality for sign-in verification

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,13 +1,47 @@
-import { betterAuth } from "better-auth"
+import { betterAuth, type GenericEndpointContext } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { emailOTP } from "better-auth/plugins"
 import { tanstackStartCookies } from "better-auth/tanstack-start"
 
 import { db } from "@/db" // your drizzle instance
+import { sendAuthOTPEmail } from "../emails/auth-otp"
+
+const OTP_LENGTH = 6
+const OTP_EXPIRATION_SECONDS = 600 // 10 minutes
+const ALLOWED_OTP_ATTEMPTS = 5
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "mysql", // or "pg", "sqlite"
 	}),
 
-	plugins: [tanstackStartCookies()], // make sure this is the last plugin in the array
+	plugins: [
+		emailOTP({
+			sendVerificationOTP,
+			otpLength: OTP_LENGTH,
+			expiresIn: OTP_EXPIRATION_SECONDS,
+			allowedAttempts: ALLOWED_OTP_ATTEMPTS,
+		}),
+		tanstackStartCookies(), // make sure this is the last plugin in the array
+	],
 })
+
+async function sendVerificationOTP(
+	data: {
+		email: string
+		otp: string
+		type: "sign-in" | "email-verification" | "forget-password"
+	},
+	_ctx?: GenericEndpointContext | undefined,
+) {
+	const { email, otp, type } = data
+	switch (type) {
+		case "sign-in": {
+			await sendAuthOTPEmail({ email, otp })
+			break
+		}
+		default: {
+			throw new Error("Unsupported OTP type. Only 'sign-in' is supported.")
+		}
+	}
+}
