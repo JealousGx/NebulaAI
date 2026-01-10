5542b06d33f06b9f061fc301755a1a19351d9a49
Abdul Mateen <59867217+JealousGx@users.noreply.github.com>
2026-01-10 14:02:22 +0500

    feat(label): add tooltip support and update styles for required fields

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,22 +1,44 @@
 "use client"
 
 import * as LabelPrimitive from "@radix-ui/react-label"
-import type * as React from "react"
+import { CircleQuestionMark } from "lucide-react"
+import * as React from "react"
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 import { cn } from "@/lib/utils"
 
-type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root>
+type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root> & {
+	tooltip?: string
+}
 
-function Label({ className, ...props }: LabelProps) {
+function Label({ className, tooltip, ...props }: LabelProps) {
+	const Container = tooltip ? "div" : React.Fragment
+
 	return (
-		<LabelPrimitive.Root
-			data-slot="label"
-			className={cn(
-				"flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-				className,
-			)}
-			{...props}
-		/>
+		<Container className="flex items-center gap-2">
+			<LabelPrimitive.Root
+				data-slot="label"
+				className={cn(
+					"flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+					className,
+				)}
+				{...props}
+			/>
+
+			<React.Activity mode={tooltip ? "visible" : "hidden"}>
+				<Tooltip>
+					<TooltipTrigger>
+						<CircleQuestionMark className="w-4 h-4" />
+					</TooltipTrigger>
+					<TooltipContent>{tooltip}</TooltipContent>
+				</Tooltip>
+			</React.Activity>
+		</Container>
 	)
 }
 
